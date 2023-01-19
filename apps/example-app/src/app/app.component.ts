150044fe1788439c626b1e30731d283fb329033f
Zachary Kipping <zjkipping@hotmail.com>
2023-01-19 08:25:26 -0600

    Generated example-app

---
+++ apps/example-app/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cypress-screenshot-issue-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'example-app';
+}
