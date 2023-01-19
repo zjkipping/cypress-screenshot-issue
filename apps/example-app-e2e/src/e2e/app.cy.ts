22004f0dac34fc72f1d5f6425de1b600af99f721
Zachary Kipping <zjkipping@hotmail.com>
2023-01-19 09:19:06 -0600

    Add cypress testing with screenshots at different resolutions

--- apps/example-app-e2e/src/e2e/app.cy.ts
+++ apps/example-app-e2e/src/e2e/app.cy.ts
@@ -1,13 +1,75 @@
-import { getGreeting } from '../support/app.po';
-
 describe('example-app', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
+  it('should take a screenshot of the full page at 1920x1080', () => {
+    cy.viewport(1920, 1080);
+    cy.screenshot('1920x1080-full-page', {
+      capture: 'fullPage',
+      overwrite: true,
+    });
+  });
 
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome example-app');
+  it('should take a screenshot of the viewport at 1920x1080', () => {
+    cy.viewport(1920, 1080);
+    cy.screenshot('1920x1080-viewport', {
+      capture: 'viewport',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the runner at 1920x1080', () => {
+    cy.viewport(1920, 1080);
+    cy.screenshot('1920x1080-runner', {
+      capture: 'runner',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the full page at ultra-wide', () => {
+    cy.viewport(3840, 1080);
+    cy.screenshot('3840x1080-full-page', {
+      capture: 'fullPage',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the viewport at ultra-wide', () => {
+    cy.viewport(3840, 1080);
+    cy.screenshot('3840x1080-viewport', {
+      capture: 'viewport',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the runner at ultra-wide', () => {
+    cy.viewport(3840, 1080);
+    cy.screenshot('3840x1080-runner', {
+      capture: 'runner',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the full page at 4k', () => {
+    cy.viewport(3840, 2160);
+    cy.screenshot('3840x2160-full-page', {
+      capture: 'fullPage',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the viewport at 4k', () => {
+    cy.viewport(3840, 2160);
+    cy.screenshot('3840x2160-viewport', {
+      capture: 'viewport',
+      overwrite: true,
+    });
+  });
+
+  it('should take a screenshot of the runner at 4k', () => {
+    cy.viewport(3840, 2160);
+    cy.screenshot('3840x2160-runner', {
+      capture: 'runner',
+      overwrite: true,
+    });
   });
 });
